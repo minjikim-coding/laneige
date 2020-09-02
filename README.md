068b16466b283380a6d27ea4a8809790e94be8f2
minjikim-coding <61400909+minjikim-coding@users.noreply.github.com>
2020-09-02 13:35:57 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# laneige
+# 💁🏻‍♀️ Introduction
+
+- 피부 수분에 대한 연구를 기초로 한 화장품 브랜드 라네즈(https://www.laneige.com/kr/ko/) 웹사이트 클론 프로젝트
+- 개발기간 : 2020.07.06 ~ 2020.07.17(약 2주)
+- 개발인원 : 3 Front-end(강솔미, 김지인, 김민지), 3 Back-end(한준수, 박준모, 양희연)
+- [Back-end Github](https://github.com/wecode-bootcamp-korea/9-Mayonnaise-backend)
+
+# 📼 Demo
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/xIFB2U80wgc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+# 👩🏻‍💻 Technologies
+
+- JavaScript(ES6)
+- React.js (CRA, Function형 component 사용(Hooks))
+- Styled Component
+- Git / Github
+
+### React 라이브러리
+
+- react-awesome-slider
+- react-dom
+- react-kakao-login
+- react-modal
+- react-rating
+- react-router-dom
+- react-script
+- react-scroll
+- react-slick
+- react-slot-machine
+- slick-carousel
+
+# 🌱 Features
+
+### 회원가입 / 로그인 페이지
+
+- 아이디 및 패스워드 유효성 검사<br/>
+  (아이디, 비밀번호 조건 확인 및 비밀번호, 비밀번호 확인 일치 여부 검사)
+- 카카오 소셜 로그인
+
+### 메인 페이지
+
+- Nav, Footer 바
+- 메인 페이지 최상단 슬라이더
+
+### 제품 리스트 페이지
+
+-
+
+### 제품 상세 페이지
+
+- React, class component, Sass로 메인, 제품 상세페이지, 리뷰페이지, 장바구니 구현
+- 이미지를 map함수를 사용하여 기능 구현
+- 리뷰를 Filter함수를 사용하여 기능 구현
+- 제품 상세페이지의 장바구니 버튼을 만들어서 상품정보를 POST로 서버에 전송하여 기능 
+   구현
+
+### 장바구니 페이지
+
+- 유저 정보 확인 후 백엔드 서버에 저장된 장바구니 데이터 불러와서 출력
+- 수량 수정, 삭제 버튼으로 장바구니 상품 수량 변경 및 삭제 기능
+
+### 브랜드 페이지 - 서비스 예약
+
+- 서비스 예약 신청시 백엔드 서버로 정보를 전송하여 예약 정보 저장
+- 서비스 예약 확인 및 취소 페이지에서 예약 취소 가능
+
+### 브랜드 페이지 - Refill Me
+
+- 최상단 동영상이 자연스럽게 전환되는 애니메이션
+- 스크롤 위치에 따라 배경색이 바뀌는 기능
---
+++ src/config.js
@@ -0,0 +1,136 @@
+// IMAGES
+//nav
+import user from "Image/Nav/user.svg";
+import pin from "Image/Nav/location.svg";
+import search from "Image/Nav/search.svg";
+import cart from "Image/Nav/shopping-cart.svg";
+import orange from "Image/Nav/global-banner-skincare.jpg";
+
+// main
+
+// brand
+import quote_icon from "Image/Brand/RefillMe/quote.svg";
+
+// PATH
+// main
+
+// login
+import warning from "Image/Login/warning.svg";
+import checked from "Image/Login/checked.svg";
+
+// detail
+import star_empty from "Image/Datail/star_empty.png";
+import star_fill from "Image/Datail/star_fill.png";
+
+// signup
+
+// PATH
+// main
+
+export const MAIN_SLIDER_IMG1 =
+  "https://www.laneige.com/kr/ko/layout/main/key-visual/__icsFiles/afieldfile/2020/06/17/20200617_neo-cushion_KV_mo_1.png";
+export const MAIN_SLIDER_IMG2 =
+  "https://www.laneige.com/kr/ko/layout/main/key-visual/__icsFiles/afieldfile/2020/04/24/cream-skin-refiner-main_kv-200424-mo_1.jpg";
+export const MAIN_SLIDER_IMG3 =
+  "https://www.laneige.com/kr/ko/layout/main/key-visual/__icsFiles/afieldfile/2020/04/14/Laneige_WBE_Main_MOB.mp4";
+export const MAIN_SLIDER_IMG4 =
+  "https://www.drmartens.co.kr/data/cms/dr_marten_pc/images/0306afdb-3095-4a2c-a160-125dcd473c23.jpg";
+export const ARROW_ICON = "http://cdn.onlinewebfonts.com/svg/img_295694.svg";
+export const STAR_EMPTY = star_empty;
+export const STAR_FILL = star_fill;
+
+// signup
+
+// EXPORT
+
+// login
+export const LOGIN_BG_SM =
+  "https://www.laneige.com/kr/ko/assets/image/login/login-bg-sm.jpg";
+export const LOGIN_BG_LG =
+  "https://www.laneige.com/kr/ko/assets/image/login/login-bg-lg.jpg";
+
+export const WARNING_ICON = warning;
+export const CHECKED_ICON = checked;
+
+export const USER = user;
+export const PIN = pin;
+export const SEARCH = search;
+export const CART = cart;
+export const ORANGE = orange;
+
+// signup
+
+// brand
+export const REFILLME_IMG =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/walter.png";
+export const QUOTE_ICON = quote_icon;
+
+export const PARTNER_IMG1 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/global-partners-0.png";
+export const PARTNER_IMG2 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/global-partners-1.png";
+export const PARTNER_IMG3 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/global-partners-2.png";
+export const PARTNER_IMG4 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/global-partners-3.png";
+export const PARTNER_IMG5 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/global-partners-4.png";
+export const PARTNER_IMG6 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/global-partners-5.png";
+
+export const TOGETHER_IMG1 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/together-slide-0.jpg";
+export const TOGETHER_IMG2 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/together-slide-1.jpg";
+export const TOGETHER_IMG3 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/together-slide-2.jpg";
+export const TOGETHER_IMG4 =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/together-slide-3.jpg";
+
+export const BOTTOM_BG =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/banner-model.png";
+
+// VIDEO
+// brand-refell me
+export const REFILLME_VIDEO =
+  "https://www.laneige.com/kr/ko/assets/video/refill-me-campaign/csr-hero.mp4";
+
+export const REFILLME_BG =
+  "https://www.laneige.com/kr/ko/assets/image/brand/refill-me-campaign/cover-promise.jpg";
+
+// signup
+
+export const REFILL_ITEM_VIDEO1 =
+  "https://www.laneige.com/kr/ko/assets/video/refill-me-campaign/csr-feature-water.mp4";
+export const REFILL_ITEM_VIDEO2 =
+  "https://www.laneige.com/kr/ko/assets/video/refill-me-campaign/csr-feature-life.mp4";
+export const REFILL_ITEM_VIDEO3 =
+  "https://www.laneige.com/kr/ko/assets/video/refill-me-campaign/csr-feature-eco.mp4";
+
+// OBJ
+// brand-flagship store
+export const crumb_position = [
+  "소개",
+  "서비스 소개",
+  "서비스 예약",
+  "예약확인 및 취소",
+];
+
+export const menu_title = [
+  "플래그십 스토어 소개",
+  "서비스 소개",
+  "서비스 예약",
+  "예약확인 및 취소",
+];
+
+// API
+//cart
+export const RESERVATION_API = "http://10.58.1.117:8000/reservation";
+
+// login, signup
+export const SIGN_API = "http://10.58.1.117:8000/account";
+
+// detail
+export const REVIEW_API = "http://10.58.1.117:8000/review";
+// cart
+export const CART_API = "http://10.58.1.117:8000/order";
